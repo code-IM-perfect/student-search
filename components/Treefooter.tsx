829502a3fd375d603e076e70ac227821a968ab65
Harshit Tomar <harshitt345@gmail.com>
2024-12-21 13:43:51 +0530

    simplified footer text
    Signed-off-by: Harshit Tomar <harshitt345@gmail.com>

--- components/Treefooter.tsx
+++ components/Treefooter.tsx
@@ -4,14 +4,15 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
 export default function GuestFooter() {
-    return (
-        <Paper sx={{marginTop: 'calc(10% + 60px)',
-        position: 'fixed',
-        bottom: 0,
-        width: '100%',
-        }} component="footer" square variant="outlined">
-        <Container maxWidth="lg">
-          {/* <Box
+  return (
+    <Paper sx={{
+      marginTop: 'calc(10% + 60px)',
+      position: 'fixed',
+      bottom: 0,
+      width: '100%',
+    }} component="footer" square variant="outlined">
+      <Container maxWidth="lg">
+        {/* <Box
             sx={{
               flexGrow: 1,
               justifyContent: "center",
@@ -21,25 +22,25 @@ export default function GuestFooter() {
           >
               
           </Box> */}
-  
-          <Box
-            sx={{
-              flexGrow: 1,
-              justifyContent: "center",
-              display: "flex",
-              alignItems: "center",
-              mb: 0.5,
-              mt: 0.5
-            }}
-          >
-            <Typography variant="caption" sx={{fontSize: '0.80rem'}}>
-                Family tree provided by <a href="https://www.iitk.ac.in/counsel/">Institute Counselling Service IITK</a>
-            </Typography>
-            <div>
-                <img src="/cslogo.png" width={18} height={18} style={{marginLeft: '8px'}} alt="Logo" />
-            </div>
-          </Box>
-        </Container>
-      </Paper>
-    );
-  }
+
+        <Box
+          sx={{
+            flexGrow: 1,
+            justifyContent: "center",
+            display: "flex",
+            alignItems: "center",
+            mb: 0.5,
+            mt: 0.5
+          }}
+        >
+          <Typography variant="caption" sx={{ fontSize: '0.80rem' }}>
+            data by <a href="https://www.iitk.ac.in/counsel/">CS</a>
+          </Typography>
+          <div>
+            <img src="/cslogo.png" width={18} height={18} style={{ marginLeft: '8px' }} alt="Logo" />
+          </div>
+        </Box>
+      </Container>
+    </Paper>
+  );
+}
